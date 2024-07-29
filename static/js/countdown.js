0b6ce00f3c7bc8b48a876157ade74942e20de3d5
Maxwel Khadambi <maxwelkhadambi@gmail.com>
2024-07-29 13:18:15 +0300

    updated

--- static/js/countdown.js
+++ static/js/countdown.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("DOM fully loaded and parsed");
 
     // Countdown timer
-    var countDownDate = new Date("Aug 16, 2024 19:00:00").getTime();
+    var countDownDate = new Date("Oct 24, 2024 12:00:00").getTime();
     var x = setInterval(function () {
         var now = new Date().getTime();
         var distance = countDownDate - now;
