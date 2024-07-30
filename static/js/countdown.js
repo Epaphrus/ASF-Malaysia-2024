1dbd50d91135a01ab6b77c2fc3a733a8f4f80b74
Maxwel Khadambi <maxwelkhadambi@gmail.com>
2024-07-30 14:51:56 +0300

    nav bar updated

--- static/js/countdown.js
+++ static/js/countdown.js
@@ -33,28 +33,6 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }, 1000);
 
-    var swiper = new Swiper(".speakers-slider", {
-        slidesPerView: 1,
-        spaceBetween: 20,
-        breakpoints: {
-            768: {
-                slidesPerView: 2,
-            },
-            1024: {
-                slidesPerView: 3,
-            },
-        },
-        loop: false,
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-    });
-
     var navbarButton = document.querySelector(".navbar-button a");
     if (navbarButton) {
         navbarButton.addEventListener("click", function (event) {
---
+++ static/js/navbarScroll.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navbar = document.querySelector('.navbar');
+    const initialColor = '#00000057';
+    const targetColor = '#123800';
+
+    // Set initial background color
+    navbar.style.backgroundColor = initialColor;
+
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 100) {
+            navbar.style.backgroundColor = targetColor;
+        } else {
+            navbar.style.backgroundColor = initialColor;
+        }
+    });
+});
