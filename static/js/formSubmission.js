370d1ffc141c79c990f005a571bc17e04121a83b
Maxwel Khadambi <maxwelkhadambi@gmail.com>
2024-07-31 12:17:44 +0300

    form validation

---
+++ static/js/formSubmission.js
@@ -0,0 +1,33 @@
+function submitForm() {
+    var form = document.getElementById('registrationForm');
+    if (form.checkValidity() === false) {
+      form.classList.add('was-validated');
+      return false;
+    }
+  
+    var formData = new FormData(form);
+    fetch('https://script.google.com/macros/s/AKfycbyOm2CAYy8366iolYF1bX7N9kYLgDf-bCZ1PM1arYXzq4EJ1eZ3z13F_OwGL2bhURvU/exec', {
+      method: 'POST',
+      body: formData
+    })
+    .then(response => response.text())
+    .then(result => {
+      alert("Form submitted successfully!");
+      form.reset();
+      disableForm();
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      alert("An error occurred. Please try again.");
+    });
+  
+    return false;
+  }
+  
+  function disableForm() {
+    var formElements = document.getElementById('registrationForm').elements;
+    for (var i = 0; i < formElements.length; i++) {
+      formElements[i].disabled = true;
+    }
+  }
+  
