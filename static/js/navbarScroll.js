849eaa7ed319351add21252fe7489decedd0af24
Maxwel Khadambi <maxwelkhadambi@gmail.com>
2024-08-07 15:48:03 +0300

    added the back to top arrow

--- static/js/navbarScroll.js
+++ static/js/navbarScroll.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const navbar = document.querySelector('.navbar');
     const initialColor = '#00000057';
     const targetColor = '#0e3100';
@@ -6,11 +6,29 @@ document.addEventListener('DOMContentLoaded', function() {
     // Set initial background color
     navbar.style.backgroundColor = initialColor;
 
-    window.addEventListener('scroll', function() {
+    window.addEventListener('scroll', function () {
         if (window.scrollY > 100) {
             navbar.style.backgroundColor = targetColor;
         } else {
             navbar.style.backgroundColor = initialColor;
         }
+    });
+});
+
+// BACK TO TOP ARROW JS
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+    // scroll body to 0px on click
+    $('#back-to-top').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
     });
 });
