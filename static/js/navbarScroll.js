9db848d131cce8df5fb5987d9c44736a3703b31a
Maxwel Khadambi <maxwelkhadambi@gmail.com>
2024-10-23 15:27:24 +0300

    major update

--- static/js/navbarScroll.js
+++ static/js/navbarScroll.js
@@ -32,26 +32,3 @@ $(document).ready(function () {
         return false;
     });
 });
-
-// SLIDE UP CONTENT ONSCROLL 
-// document.addEventListener('DOMContentLoaded', function() {
-//     var fadeElements = document.querySelectorAll('.fade-in-up');
-
-//     function checkFade() {
-//         fadeElements.forEach(function(element) {
-//             var elementRect = element.getBoundingClientRect();
-//             var elementHeight = elementRect.height;
-//             var visibleHeight = Math.min(elementHeight, window.innerHeight - elementRect.top);
-//             var visiblePercentage = (visibleHeight / elementHeight) * 100;
-
-//             if (visiblePercentage >= 50) {
-//                 element.classList.add('is-visible');
-//             }
-//         });
-//     }
-
-//     window.addEventListener('scroll', checkFade);
-//     window.addEventListener('resize', checkFade);
-//     checkFade();
-// });
-
